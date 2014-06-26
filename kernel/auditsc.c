919f2274e4679fb50c7c3f2c24133524c1e2c9b3
Dorimanx <yuri@bynet.co.il>
2014-06-26 19:19:59 +0300

    Fixes to kernel code inits after checking build with -O3
    Conflicts:
    	arch/arm/mach-msm/clock-generic.c
    	fs/namei.c
    	kernel/auditsc.c
    	kernel/cgroup.c
    	kernel/sysctl.c
    	net/socket.c
    	sound/usb/mixer.c
    
    Change-Id: I9e876a462486b45a3e2d33256a2cec91487261eb

--- kernel/auditsc.c
+++ kernel/auditsc.c
@@ -927,9 +927,9 @@ static int audit_filter_inode_name(struct task_struct *tsk,
 	struct audit_entry *e;
 	enum audit_state state = 0;
 
+
 	if (list_empty(list))
 		return 0;
-
 	list_for_each_entry_rcu(e, list, list) {
 		if (audit_in_mask(&e->rule, ctx->major) &&
 		    audit_filter_rules(tsk, &e->rule, ctx, n, &state, false)) {
