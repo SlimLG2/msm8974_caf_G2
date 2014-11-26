5d474c41061d40848a2f316f3b715a319e7ddcf6
Adrian Salido-Moreno <adrianm@codeaurora.org>
2014-11-26 09:49:01 -0800

    msm: mdss: free ion memory after panel is blanked
    Frame buffer memory could still be fetched until panel is completely
    blanked, in order to avoid potential page faults by unmapping early,
    move unmapping of this memory until after panel is blanked.
    
    CRs-Fixed: 763046
    Change-Id: Ib7eea73a7549b453cf2fc31a31f5cc3e9e1cce39
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>
    Signed-off-by: Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_fb.c

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1983,9 +1983,6 @@ static int mdss_fb_release_all(struct fb_info *info, bool release_all)
 					mfd->index, task->comm, pid);
 		}
 
-		if (mfd->fb_ion_handle)
-			mdss_fb_free_fb_ion_memory(mfd);
-
 		ret = mdss_fb_blank_sub(FB_BLANK_POWERDOWN, info,
 			mfd->op_enable);
 		if (ret) {
@@ -1993,6 +1990,10 @@ static int mdss_fb_release_all(struct fb_info *info, bool release_all)
 				mfd->index, ret, task->comm, pid);
 			return ret;
 		}
+
+		if (mfd->fb_ion_handle)
+			mdss_fb_free_fb_ion_memory(mfd);
+
 		atomic_set(&mfd->ioctl_ref_cnt, 0);
 	}
 
