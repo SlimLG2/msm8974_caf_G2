be55b9e87542e3b8fd19a59b27cf385d19b27393
Venkat kamesh <venkat.kamesh09@gmail.com>
2015-05-12 01:53:36 +0530

    random: Increase entropy_avail wakeup thresholds

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -299,8 +299,9 @@
 #ifdef CONFIG_CRYPTO_FIPS
 static int random_read_wakeup_thresh = 256;
 #else
-static int random_read_wakeup_thresh = 64;
+static int random_read_wakeup_thresh = 256;
 #endif
+
 /*
  * If the entropy count falls under this number of bits, then we
  * should wake up processes which are selecting or polling on write
@@ -309,7 +310,7 @@ static int random_read_wakeup_thresh = 64;
 #ifdef CONFIG_CRYPTO_FIPS
 static int random_write_wakeup_thresh = 320;
 #else
-static int random_write_wakeup_thresh = 128;
+static int random_write_wakeup_thresh = 512;
 #endif
 
 /*
